2dfd51e7ae14bcb35ef2e86ab644508f35c0ab53
bhavyakarimikonda <bhavyakarimikonda@192.168.1.6>
2023-05-18 16:26:39 +0530

    First commit - added project

---
+++ TestngProj/src/test/java/testngMain.java
@@ -0,0 +1,49 @@
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class testngMain{
+
+		WebDriver driver = null;
+	
+		@BeforeMethod
+		public void setUpTest() {
+			driver = new ChromeDriver();
+			
+			ChromeOptions options = new ChromeOptions();
+			
+			options.addArguments("start-maximized");
+		    
+			driver = new ChromeDriver(options);
+			
+		}
+		
+		@Test
+		public void gotoPage() {
+			  driver.get("https://www.bstackdemo.com");
+		      AssertJUnit.assertTrue(driver.getTitle().matches("StackDemo"));
+		}
+		
+		@Test
+		public void addToCart() {
+			 driver.get("https://www.bstackdemo.com");
+			 driver.findElement(By.xpath("//*[@id=\"1\"]/div[4]")).click(); 
+			 AssertJUnit.assertTrue(driver.findElement(By.cssSelector(".float\\-cart__content")).isDisplayed());
+			 
+		}
+		
+		
+		@AfterMethod
+		public void cleanUp() {
+			driver.quit();
+		}
+		
+	
+}
